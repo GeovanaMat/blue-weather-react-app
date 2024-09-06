dbaa98b5774ed3e7efdd28be6b13f985d5ef0034
Geovana Matias R <120607451+GeovanaMat@users.noreply.github.com>
2024-09-06 13:30:34 -0400

    Modificando vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blue-weather-react-app"
+  base: "/blue-weather-react-app/"
 })
